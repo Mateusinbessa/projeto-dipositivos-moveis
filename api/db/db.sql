cdf7b695ffd96ad136e01fa6040c06a2956bf7a8
Mateusinbessa <mateusbessacontato@gmail.com>
2024-11-12 00:04:29 -0300

    atualizando o banco de dados

--- api/db/db.sql
+++ api/db/db.sql
@@ -12,5 +12,10 @@ CREATE TABLE despesas(
   description VARCHAR(200) NOT NULL
 );
 
-INSERT INTO public.receitas  (date, amount, description) VALUES ('30/05/2024', 100, 'Receita de aniversário');
-INSERT INTO public.despesas  (date, amount, description) VALUES ('30/05/2024', 80, 'Despesa de aniversário');
+insert into public.receitas  (date, amount, description) values ('30/05/2003', 100, 'Receita de aniversário');
+insert into public.receitas  (date, amount, description) values ('29/10/2005', 100, 'Receita de nascimento');
+insert into public.receitas  (date, amount, description) values ('12/11/1980', 100, 'Receita de médicos');
+
+insert into public.despesas  (date, amount, description) values ('30/05/2003', 80, 'Despesa de aniversário');
+insert into public.despesas  (date, amount, description) values ('29/10/2005', 80, 'Despesa de nascimento');
+insert into public.despesas  (date, amount, description) values ('12/11/1980', 80, 'Despesa de médicos');
